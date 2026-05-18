0a03d6c79abaa5f5441a888d0734d7a15a8230e2
Rodrigo <rodrioppo2006@gmail.com>
2026-05-18 14:16:31 +0200

    navBar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
-    <h1 className="font-extrabold underline">hello there</h1>
+      <Navbar/>
     </>
   );
 }
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import { navLinks, navIcons } from '@/constants'
+import dayjs from 'dayjs'
+import 'dayjs/locale/es';
+
+dayjs.locale('es');
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <nav>
+        <div>
+            <img src="/images/logo.svg" alt="logo" />
+            <p className='font-bold'>Archivador online UwU</p>
+
+            <ul>
+              {navLinks.map(({id, name})=>(
+                <li key={id}>
+                  <p>{name}</p>
+                </li>
+              ))}
+            </ul>
+        </div>
+
+
+        <div>
+          <ul>
+              {navIcons.map(({id, img})=> (
+                <li key={id}>
+                  <img src={img} alt={`icon-${id}`} className='icon-hover' />
+                </li>
+              ))}
+          </ul>
+
+          <time>{dayjs().format("ddd MMM D YYYY H:mm")}</time>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
